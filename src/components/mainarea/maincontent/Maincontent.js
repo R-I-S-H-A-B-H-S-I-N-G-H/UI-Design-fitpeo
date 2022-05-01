44fcfc71b6a1e16fdc25eacd99a7fc90dd71492d
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2022-05-01 17:28:46 +0530

    final changes

--- src/components/mainarea/maincontent/Maincontent.js
+++ src/components/mainarea/maincontent/Maincontent.js
@@ -1,29 +1,52 @@
 import "./maincontent.css";
 import girl from "../../../assets/girl.png";
 function Maincontent(params) {
-  return (
-    <div className="main-content">
-      <div className="welcome">
-        <div className="dashbord">
-          <div className="greetings">
-            <h2>Welcome back!</h2>
-            <h2>User</h2>
-            <p>Reprehenderit mollit aute cillum dolor exercitation sunt</p>
-            <button>Go Now</button>
-          </div>
-          <div className="box">
-            <img src={girl}></img>
-          </div>
-        </div>
-        <div className="features">
-          <img src="https://images.pexels.com/photos/1535972/pexels-photo-1535972.jpeg?auto=compress&cs=tinysrgb&h=200&w=300"></img>
-        </div>
-      </div>
-      <div className="dvdr"></div>
-      <div className="activity">
-        
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-content">
+			<div className="welcome">
+				<div className="dashbord">
+					<div className="greetings">
+						<h2>Welcome back!</h2>
+						<h2>User</h2>
+						<p>Reprehenderit mollit aute cillum dolor exercitation sunt</p>
+						<button>Go Now</button>
+					</div>
+					<div className="box">
+						<img src={girl}></img>
+					</div>
+				</div>
+				<div className="features">
+					<img src="https://images.pexels.com/photos/1535972/pexels-photo-1535972.jpeg?auto=compress&cs=tinysrgb&h=200&w=300"></img>
+				</div>
+			</div>
+			<div className="dvdr"></div>
+			<div className="activity">
+				<div className="first">
+					<div className="totalactive">
+						<div>Total Active Users</div>
+						<h4>+2.4%</h4>
+						<h1>18,765</h1>
+					</div>
+					<div className="totalinstall">
+						<div>Total Installs</div>
+						<h4>+2.4%</h4>
+						<h1>18,765</h1>
+					</div>
+				</div>
+				<div className="second">
+					<div className="totaldown">
+						<div>Total Active Downloads</div>
+						<h4>+2.4%</h4>
+						<h1>18,765</h1>
+					</div>
+					<div className="totalrevenue">
+						<div>Total Revenue</div>
+						<h4>+2.4%</h4>
+						<h1>18,765</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Maincontent;
--- src/components/mainarea/navbar/Navbar.js
+++ src/components/mainarea/navbar/Navbar.js
@@ -1,16 +1,21 @@
 import "./navbar.css";
+import searchPng from "../../../assets/search.png";
+import bell from "../../../assets/bell.png";
+import menu from "../../../assets/menu.png";
+import group from "../../../assets/group.png";
+
 function Navbar(params) {
-  return (
-    <div className="navbar-area">
-      <div className="left">
-        <img src="https://cdn-icons.flaticon.com/png/512/2811/premium/2811790.png?token=exp=1650975345~hmac=3ff9ed5a68ec7dccf13aad7bffe19ec7"></img>
-        <img src="https://cdn-icons-png.flaticon.com/512/130/130918.png"></img>
-      </div>
-      <div className="right">
-        <img src="https://cdn-icons-png.flaticon.com/512/1827/1827422.png"></img>
-        <img src="https://cdn-icons-png.flaticon.com/512/681/681494.png"></img>
-      </div>
-    </div>
-  );
+	return (
+		<div className="navbar-area">
+			<div className="left">
+				<img src={searchPng}></img>
+				<img src={menu}></img>
+			</div>
+			<div className="right">
+				<img src={bell}></img>
+				<img src={group}></img>
+			</div>
+		</div>
+	);
 }
 export default Navbar;
